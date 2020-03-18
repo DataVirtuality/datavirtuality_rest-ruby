8f7ef17e9e5cbe04370c76ce5e2fa71b26b94db6
Niklas Schmidtmer <niklas.schmidtmer@datavirtuality.com>
2020-03-18 13:56:48 +0100

    Use an explicitly named subject

--- spec/datavirtuality/rest_spec.rb
+++ spec/datavirtuality/rest_spec.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 describe DataVirtuality::Rest, :vcr do
-  subject { described_class.new(ENV['DV_HOST'], ENV['DV_USER_NAME'], ENV['DV_PASSWORD']) }
+  subject(:client) { described_class.new(ENV['DV_HOST'], ENV['DV_USER_NAME'], ENV['DV_PASSWORD']) }
 
   describe '#status' do
     it 'returns server status' do
-      expect(subject.status).to eq(:ok)
+      expect(client.status).to eq(:ok)
     end
   end
 
   describe '#data_sources' do
     it 'returns data sources' do
-      expect(subject.data_sources).to eq(%w[SYS SYSADMIN UTILS pg_catalog views minter_examples similarweb similarweb_examples])
+      expect(client.data_sources).to eq(%w[SYS SYSADMIN UTILS pg_catalog views minter_examples similarweb similarweb_examples])
     end
   end
 
@@ -20,11 +20,11 @@ describe DataVirtuality::Rest, :vcr do
     let(:nonexisting_table) { 'Something/Does/Not/Exist' }
 
     it 'returns all rows of a given table' do
-      expect(subject.get(existing_table)).to eq([{ Name: 'datavirtuality', Version: '1' }])
+      expect(client.get(existing_table)).to eq([{ Name: 'datavirtuality', Version: '1' }])
     end
 
     it 'throws an exception' do
-      expect { subject.get(nonexisting_table) }.to raise_error(DataVirtuality::RestException)
+      expect { client.get(nonexisting_table) }.to raise_error(DataVirtuality::RestException)
     end
   end
 
@@ -33,11 +33,11 @@ describe DataVirtuality::Rest, :vcr do
     let(:incorrect_query) { 'SELECT * FROM SYS.DoesNotExist' }
 
     it 'returns all rows of a given query' do
-      expect(subject.query(correct_query)).to eq([{ Name: 'datavirtuality', Version: '1' }])
+      expect(client.query(correct_query)).to eq([{ Name: 'datavirtuality', Version: '1' }])
     end
 
     it 'throws an exception' do
-      expect { subject.query(incorrect_query) }.to raise_error(DataVirtuality::RestException)
+      expect { client.query(incorrect_query) }.to raise_error(DataVirtuality::RestException)
     end
   end
 end
